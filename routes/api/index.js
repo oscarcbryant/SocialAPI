5a713fa8960968a91ecd7351a430f39a52788388
Oscar Bryant <oscarcbryant@gmail.com>
2022-04-07 07:24:21 +1000

    adding routes

---
+++ routes/api/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const thoughtRoutes = require('./thoughtRoutes');
+
+router.use('/Users', userRoutes);
+router.use('/Thoughts', thoughtRoutes);
+
+module.exports = router;
---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const {
+    getThought,
+    getSingleThought,
+    createThought,
+    updateThought,
+    removeThought,
+    createReaction,
+    removeReaction
+  } = require('../../controllers/thoughtController.js');
+  
+  // /api/courses
+  router.route('/').get(getThought);
+  
+  // /api/courses/:courseId
+  router
+    .route('/:thoughtId')
+    .get(getSingleThought)
+    .post(createThought)
+    .put(updateThought)
+    .delete(removeThought);
+
+    router.route('/:thoughtId/reactions').post(createReaction).delete(removeReaction);
+  
+  module.exports = router;
+  
